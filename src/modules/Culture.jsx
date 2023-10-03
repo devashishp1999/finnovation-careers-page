294f0c763eb0ffd7d035b719318ebb805b613142
devashish <devashish@unstop.com>
2023-10-03 22:08:41 +0530

    uis

--- src/modules/Culture.jsx
+++ src/modules/Culture.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import useScreenSize from "../hooks/useScreenSize";
 
 const imgURL = "https://random.imagecdn.app/800/800";
 
 const Culture = () => {
+  const viewport = useScreenSize();
+
   return (
     <section className="culture">
       <div className="container">
@@ -13,23 +16,55 @@ const Culture = () => {
         </p>
         <button>Explore more</button>
 
-        <div className="images">
-          <div className="img">
-            <img src={imgURL+"?1"}/>
+        {viewport === "desktop" ? (
+          <div className={"images " + viewport}>
+            <div className="parts" data-d="50-50">
+              <div className="img">
+                <img src={imgURL + "?1"} />
+              </div>
+              <div className="img">
+                <img src={imgURL + "?2"} />
+              </div>
+            </div>
+
+            <div className="parts" data-d="25-50-25">
+              <div className="img">
+                <img src={imgURL + "?3"} />
+              </div>
+              <div className="img">
+                <img src={imgURL + "?4"} />
+              </div>
+              <div className="img">
+                <img src={imgURL + "?5"} />
+              </div>
+            </div>
           </div>
-          <div className="img">
-            <img src={imgURL+"?2"}/>
+        ) : (
+          <div className={"images " + viewport}>
+            <div className="parts" data-d="60-40">
+              <div className="img">
+                <img src={imgURL + "?1"} />
+              </div>
+              <div className="img">
+                <img src={imgURL + "?2"} />
+              </div>
+            </div>
+            <div className="parts" data-d="40-60">
+              <div className="img">
+                <img src={imgURL + "?1"} />
+              </div>
+              <div className="img">
+                <img src={imgURL + "?2"} />
+              </div>
+            </div>
+
+            <div className="parts" data-d="100">
+              <div className="img">
+                <img src={imgURL + "?3"} />
+              </div>
+            </div>
           </div>
-          <div className="img">
-            <img src={imgURL+"?3"}/>
-          </div>
-          <div className="img">
-            <img src={imgURL+"?4"}/>
-          </div>
-          <div className="img">
-            <img src={imgURL+"?9"}/>
-          </div>
-        </div>
+        )}
       </div>
     </section>
   );
