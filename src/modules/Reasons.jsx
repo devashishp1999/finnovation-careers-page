e408b8c83048530564483daeb9f62f78bcd2c542
devashish <devashish@unstop.com>
2023-10-21 17:07:18 +0530

    reasons

--- src/modules/Reasons.jsx
+++ src/modules/Reasons.jsx
@@ -1,39 +1,20 @@
 import React from "react";
-import { IMAGES } from "../assets/assets";
 import Reason from "../components/Reason";
+import { reasonsToJoin } from "../resources/data.json";
 
 const Reasons = () => {
-  const reasons = [
-    {
-      icon: IMAGES.accomodation,
-      title: "Save Your Maximum Salary",
-      desc: "Free accomodation.",
-    },
-    {
-      icon: IMAGES.build,
-      title: "Join Our “Build From Rural Mission”",
-      desc: "Work With Nature.",
-    },
-    {
-      icon: IMAGES.salary,
-      title: "Best in the Industry Salary.",
-      desc: "Best package in the market.",
-    },
-    {
-      icon: IMAGES.days,
-      title: "5 Days Work Week.",
-      desc: "Saturday and Sunday off.",
-    },
-  ];
-
   return (
     <section className="reasons">
       <div className="container">
-        <h2>Top 4 Reasons on why you<br/>should join us?</h2>
+        <h2>
+          Top 4 Reasons on why you
+          <br />
+          should join us?
+        </h2>
         <hr />
 
         <div className="cards">
-          {reasons.map((reason, i) => (
+          {reasonsToJoin.map((reason, i) => (
             <Reason key={i} data={reason} />
           ))}
         </div>
