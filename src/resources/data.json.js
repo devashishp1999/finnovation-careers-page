a1579cbabfeee6142c22f3164d114a00e41d2681
devashish <devashish@unstop.com>
2023-12-09 21:26:07 +0530

    images update

--- src/resources/data.json.js
+++ src/resources/data.json.js
@@ -27,34 +27,36 @@ export const companyCultureImages = [
   "https://websitevideos2020.s3.ap-south-1.amazonaws.com/career-web-site-immages/IMG_20230803_212056.jpg",
 ];
 
+const img = (name) => name && require(`../assets/testimonial_images/${name}`);
+
 export const testimonials = [
   {
     id: 1,
-    avatar: "" || IMAGES.userImg,
-    name: "omkar korke",
-    location: "India",
-    text: " A positive and inclusive company culture. A good work-life balance. Organizations foster a culture of innovation and creativity and last one job satisfaction",
-    stars: 4,
-  },
-  {
-    id: 2,
-    avatar: "" || IMAGES.userImg,
-    name: "Vishwanath Kshirsagar",
-    location: "India",
-    text: "Working at Finnovation has been an incredibly rewarding experience. The supportive culture, opportunities for growth, and emphasis on innovation make it an ideal place to build a career. I've had the chance to collaborate with talented colleagues and contribute to impactful projects is great journey, and i am enjoying this journey.",
-    stars: 3,
-  },
-  {
-    id: 3,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("prithwish_chakraborty.jpg") || IMAGES.userImg,
     name: "Prithwish Chakraborty",
     location: "India",
     text: "The work environment is quite cool here. Everyone here is quite supportive and even contributes when you are stuck at your work. It's fun working here. You can also play games here after you are done with your work.",
     stars: 5,
   },
   {
+    id: 2,
+    avatar: img("samiran_subba.jpg") || IMAGES.userImg,
+    name: "Samiran Subba ",
+    location: "India",
+    text: "FinnovationZ is probably the best place to work, learn and grow.",
+    stars: 5,
+  },
+  {
+    id: 3,
+    avatar: img("vishwanath_kshirsagar.jpg") || IMAGES.userImg,
+    name: "Vishwanath Kshirsagar",
+    location: "India",
+    text: "Working at Finnovation has been an incredibly rewarding experience. The supportive culture, opportunities for growth, and emphasis on innovation make it an ideal place to build a career. I've had the chance to collaborate with talented colleagues and contribute to impactful projects is great journey, and i am enjoying this journey.",
+    stars: 3,
+  },
+  {
     id: 4,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("ritam_roy.jpeg") || IMAGES.userImg,
     name: "Ritam Roy",
     location: "India",
     text: "I have been a proud member of this company for over two years, and I can confidently say that it's been an incredible journey. This organization has created a nurturing and dynamic environment where employees can thrive. Also the organization's unwavering support, dedication to employee growth, and commitment to innovation have been truly commendable. Moreover, our flat organizational structure, where there is no hierarchy, fosters an atmosphere of openness and encourages everyone's input. That's why I am grateful to be part of a company so here's to many more years of success and growth together!",
@@ -62,15 +64,15 @@ export const testimonials = [
   },
   {
     id: 5,
-    avatar: "" || IMAGES.userImg,
-    name: "Samiran Subba ",
+    avatar: img("") || IMAGES.userImg,
+    name: "omkar korke",
     location: "India",
-    text: "FinnovationZ is probably the best place to work, learn and grow.",
-    stars: 5,
+    text: " A positive and inclusive company culture. A good work-life balance. Organizations foster a culture of innovation and creativity and last one job satisfaction",
+    stars: 4,
   },
   {
     id: 6,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("praveen_kumar.jpg") || IMAGES.userImg,
     name: "Praveen Kumar Pilli",
     location: "India",
     text: "I love the work culture the management is handling and it is supportive and respectful. Office culture gives us the best way to handle and  maintain a balance between work and personal life. As an employee I saw good career growth in my work experience. Company provides the benefits and encourages the new talent. ",
@@ -78,7 +80,7 @@ export const testimonials = [
   },
   {
     id: 7,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("") || IMAGES.userImg,
     name: "Armando Sallavanti",
     location: "India",
     text: "Working with full freedom and creativity ",
@@ -86,7 +88,7 @@ export const testimonials = [
   },
   {
     id: 8,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("amit_tripathi.jpg") || IMAGES.userImg,
     name: "Amit Tripathi",
     location: "India",
     text: "The most helpful course I've ever taken. Clears out the noise. Focuses you in on absolute essentials. Priceless.",
@@ -94,7 +96,7 @@ export const testimonials = [
   },
   {
     id: 9,
-    avatar: "" || IMAGES.userImg,
+    avatar: img("amir_rizwan.png") || IMAGES.userImg,
     name: "Amir Rizwan",
     location: "India",
     text: "Learning Opportunity, Personal Growth, Stress Free Environment, Supportive Team Member, Value Our Suggestion, Fun with Team Members and many more.",
